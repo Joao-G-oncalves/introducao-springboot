62d079881831edf3629da5114f0cbd783e18adac
João Gonçalves da Silva Júnior <zaquir01@gmail.com>
2024-07-25 20:02:39 -0300

    add Component Calculadora

---
+++ primeiros-passos/src/main/java/com/dio/primeiros_passos/Calculadora.java
@@ -0,0 +1,12 @@
+package com.dio.primeiros_passos;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculadora {
+    
+    public int somar(int numero1, int numero2){
+        return numero1 + numero2;
+    }
+    
+}
--- primeiros-passos/src/main/java/com/dio/primeiros_passos/PrimeirosPassosApplication.java
+++ primeiros-passos/src/main/java/com/dio/primeiros_passos/PrimeirosPassosApplication.java
@@ -8,6 +8,7 @@ public class PrimeirosPassosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeirosPassosApplication.class, args);
+
 	}
 
 }
